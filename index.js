1097973a6eff3df1098f5e25cadec1b12aa7590f
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-06 01:46:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  
+ var cantHear = "I can't hear you!";
+ var hypeGranny = "YES INDEED!";
+ var loveGranny = "I love you, too.";
+ 
+ if (string.toLowerCase(string) === string) {
+   return cantHear;
+ }
+ else if (string.toUpperCase(string) === string) {
+   return hypeGranny 
+ }
+ else if ("I love you, Grandma." === string) {
+   return loveGranny;
+ }
+ 
+ 
+ 
+}
